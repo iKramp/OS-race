209d9c5429f6143c5fc493f1985bed45e5f71f32
Nejc Sušnik <nejc.susnik05@gmail.com>
2024-04-28 12:15:30 +0200

    a much faster sscreen clear

--- build.rs
+++ build.rs
@@ -9,11 +9,15 @@ fn main() {
 
     // create an UEFI disk image (optional)
     let uefi_path = out_dir.join("uefi.img");
-    bootloader::UefiBoot::new(&kernel).create_disk_image(&uefi_path).unwrap();
+    bootloader::UefiBoot::new(&kernel)
+        .create_disk_image(&uefi_path)
+        .unwrap();
 
     // create a BIOS disk image
     let bios_path = out_dir.join("bios.img");
-    bootloader::BiosBoot::new(&kernel).create_disk_image(&bios_path).unwrap();
+    bootloader::BiosBoot::new(&kernel)
+        .create_disk_image(&bios_path)
+        .unwrap();
 
     // pass the disk image paths as env variables to the `main.rs`
     println!("cargo:rustc-env=UEFI_PATH={}", uefi_path.display());
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,9 +1,12 @@
 #![no_std]
 #![no_main]
 
-use core::{borrow::BorrowMut, panic::PanicInfo};
+use bootloader_api::{entry_point, BootloaderConfig};
+use core::panic::PanicInfo;
 
-use bootloader_api::{config::Mapping, entry_point, BootloaderConfig};
+use crate::vga_driver::init_vga_driver;
+
+mod vga_driver;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -18,8 +21,6 @@ pub static BOOTLOADER_CONFIG: BootloaderConfig = {
 
 entry_point!(kernel_main, config = &BOOTLOADER_CONFIG);
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 fn kernel_main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
     let binding = boot_info.framebuffer.as_mut().unwrap();
@@ -27,22 +28,16 @@ fn kernel_main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
         binding.info().pixel_format,
         bootloader_api::info::PixelFormat::Bgr
     );
-    let width = binding.info().width;
-    let height = binding.info().height;
-    let stride = binding.info().stride;
-    let bytes_per_pixel = binding.info().bytes_per_pixel;
-    let framebuffer = binding.buffer_mut();
+    init_vga_driver(
+        binding.info().width,
+        binding.info().height,
+        binding.info().stride,
+        binding.info().bytes_per_pixel,
+        binding.buffer_mut().as_mut_ptr(),
+    );
 
-    for y in 0..height {
-        for x in 0..width {
-            for i in 0..bytes_per_pixel {
-                let offset = (y * stride + x) * bytes_per_pixel + i;
-                unsafe {
-                    *framebuffer.as_mut_ptr().add(offset) = 0;
-                }
-            }
-        }
-    }
+    vga_driver::clear_screen();
 
+    #[allow(clippy::empty_loop)]
     loop {}
 }
